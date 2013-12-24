e8c386d3ae9403efa8105b62df0298e57769e608
Merijn Bertels <merijn.bertels@gmail.com>
2013-12-24 17:37:21 +0100

    add notes and test documents

--- koenig/backends/google_backend.py
+++ koenig/backends/google_backend.py
@@ -1,7 +1,14 @@
 from .generic import PrintBackend
 
 class GoogleCloudPrintBackend(PrintBackend):
+    """
+        Google Cloud Backend
 
+        Notes on Google Backend
+        -----------------------
+        * authentication with oauth2 keys
+        * please make all http calls with the requests lib http://docs.python-requests.org/en/latest/
+    """
     name = 'Google Cloud Print'
     slug = 'google-cloud-print'
 
--- koenig/management/commands/register_printer.py
+++ koenig/management/commands/register_printer.py
@@ -4,5 +4,9 @@ class Command(BaseCommand):
     help = 'Register a new CloudPrinter'
 
     def handle(self, *args, **options):
+        # step one  : get list of all printers from backend
+        # step two  : select printer to install
+        # step three: copy printer settings to from backend to model
+        # these functions can only be controlled by the commandline
 
         self.stdout.write('Successfully add a CloudPrinter')
